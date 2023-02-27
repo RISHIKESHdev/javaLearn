33edc9c0bbb73cdecf6d5cebfc66943d71388413
RISHIKESHdev <rishikeshhappydeveloper@gmail.com>
2023-02-27 22:02:10 +0530

    leet

---
+++ leet_code/leet13.java
@@ -0,0 +1,33 @@
+package leet_code;
+import java.util.Scanner;
+public class leet13 {
+    public static void main(String[] args){
+        String s;
+        Scanner scanner = new Scanner(System.in);
+        s=scanner.nextLine();
+        scanner.close();
+        System.out.println(romanToInt(s));
+    }
+    public static int romanToInt(String s) {
+        int ans=0,prev=0,num=0;
+        for(int i = s.length()-1; i>=0; i--){
+            switch(s.charAt(i)){
+                case 'I':num=1; break;
+                case 'V':num=5; break;
+                case 'X':num=10; break;
+                case 'L':num=50; break;
+                case 'C':num=100; break;
+                case 'D':num=500; break;
+                case 'M':num=1000; break;
+            }
+            if(num<prev){
+            ans=ans-num;
+        }
+        else{
+            ans=ans+num;
+        }
+        prev=num;
+        }
+        return ans;
+    }
+}
